17363b355f6700efe32584ba197aecc10cf68055
Alan Gomez <157183000+KleperForest@users.noreply.github.com>
2024-05-10 17:37:26 -0600

    Inicio

---
+++ Final_Project/Final_Project/main.c
@@ -0,0 +1,18 @@
+/*
+ * Final_Project.c
+ *
+ * Created: 5/10/2024 5:37:15 PM
+ * Author : samue
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
